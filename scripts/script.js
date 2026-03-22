0b22ba1fef7463ef8d3163441cbe5b4bfffa043b
carbonicality <215186372+carbonicality@users.noreply.github.com>
2026-03-22 16:03:11 +0000

    update: main script to allow page nav properly

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 lucide.createIcons();
+
+// the pages
+const views = {
+    Home: 'home-view',
+    Games: 'games-view',
+    Browser: 'browser-view'
+};
+
 const buttons = document.querySelectorAll('.sidebar-btn');
 buttons.forEach(btn => {
     btn.addEventListener('click',()=>{
@@ -9,8 +17,12 @@ buttons.forEach(btn => {
 
 document.querySelectorAll('.sidebar-btn').forEach(btn => {
     btn.addEventListener('click',() => {
-        if (btn.dataset.label==='Games') window.location.href='./pages/games.html';
-        if (btn.dataset.label==='Home') window.location.href='./index.html';
+        const label = btn.dataset.label;
+        document.querySelectorAll('.sidebar-btn').forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+        document.querySelectorAll('.content').forEach(v => v.classList.add('hidden'));
+        const target = document.getElementById(views[label]);
+        if (target) target.classList.remove('hidden');
     });
 });
 
